94831ca020745b91a0df8854e56e490f4cff166e
Anand Parayan <123826255+Anand-bit20@users.noreply.github.com>
2023-09-07 14:57:08 +0530

    Add files via upload

---
+++ linear_regression.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Load the dataset
+data = pd.read_csv("train.csv")
+
+# Select features and target variable
+features = ['GrLivArea', 'BedroomAbvGr', 'FullBath']
+target = 'SalePrice'
+
+X = data[features]
+y = data[target]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Calculate the mean squared error
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+# Visualize predictions vs. actual values
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual Prices vs. Predicted Prices")
+plt.show()
