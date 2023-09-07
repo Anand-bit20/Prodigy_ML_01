f4d8d5c58e9ee8563ba4cb3d097f86931f2da92e
Anand Parayan <123826255+Anand-bit20@users.noreply.github.com>
2023-09-07 15:13:58 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -15,6 +15,10 @@ target = 'SalePrice'
 X = data[features]
 y = data[target]
 
+# Handle missing values if any
+X = X.fillna(0)
+y = y.fillna(0)
+
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
